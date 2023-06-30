3663fdfb51f57d33f8412980af970243b5c975c3
Ajibade <bade@summari.com>
2023-06-30 12:49:22 -0700

    updates

--- agent-scripts.js
+++ agent-scripts.js
@@ -1,14 +1,22 @@
 import { DelegatorAgent } from "@llynxai/agents";
 import axios from "axios";
 import fs from "fs/promises";
+import PromptSync from "prompt-sync";
+
+const prompt = PromptSync({ sigint: true })
 
 
 const main = async () => {
+
+  const query = prompt("What would you like for me to schedule for you? ")
+
+  console.log("I will be happy to do that for you!\n")
+
   // Call the llynx api to get an action plan
   console.log("Getting action plan...")
   const res = await axios.post(
     "https://api.llynx.ai/actions/quickstart",
-    { query: "Schedule a meeting with Ed tomorrow at noon." },
+    { query },
     {
       headers: {
         "x-api-key": "OkiPGfAeKK88cqZlSOzbg6HhZgmlw61eam3aq6P1",
